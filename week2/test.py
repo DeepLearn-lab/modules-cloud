5a1aae24874c28f2d995c9af945f408c0b4fa74f
GauravBh1010tt <gauravbhatt.deeplearn@gmail.com>
2018-02-28 14:48:08 +0530

    new commit

---
+++ week2/test.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+port = int(os.getenv("PORT"))
+
+@app.route('/')
+def hello_world():
+    return 'Welcome'
+
+if __name__ == '__main__':
+    #port = 1245
+    app.run(host='0.0.0.23',port=port)
