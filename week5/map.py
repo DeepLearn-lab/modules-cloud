04e6d36e79f1b0378cc06bc367e98fd41e834700
Gaurav Bhatt <gauravbhatt.deeplearn@gmail.com>
2018-04-20 14:22:09 +0530

    Add files via upload

---
+++ week5/map.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+# Format of each line is:
+# date\ttime\tstore name\titem description\tcost\tmethod of payment
+#
+# We want elements 2 (store name) and 4 (cost)
+# We need to write them out to standard output, separated by a tab
+
+import sys
+
+for line in sys.stdin:	
+	d = line.split()
+	for i in d:
+		print "{0}\t{1}".format(i,1)
